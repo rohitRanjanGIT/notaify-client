758059da855c04d1b79080baae9416b448ffc5da
rohit ranjan <rrohit2005@gmail.com>
2026-02-02 16:48:27 +0530

    setup_init

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,22 +1,25 @@
 export interface Project {
-    id: string;
-    name: string;
-    description: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  user_id: string;
+  name: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface ProjectConfig {
-    id: string;
-    projectName: string;
-    llmType: "openai" | "claude" | "google" | "";
-    llmApiKey: string;
-    llmApiModel: string;
-    smtpUser: string;
-    smtpPass: string;
-    emailFrom: string;
-    emailTo: string;
-    notaifyApiKey?: string;
-    createdAt?: string;
-    updatedAt?: string;
+  projectName: string;
+
+  llmType?: "openai" | "claude" | "google" | "";
+  llmApiKey?: string;
+  llmApiModel?: string;
+
+  smtpUser?: string;
+  smtpPass?: string;
+  emailFrom?: string;
+  emailTo?: string;
+
+  notaifyApiKey?: string;
 }
+
+export type ProjectWithConfig = Project & ProjectConfig;
