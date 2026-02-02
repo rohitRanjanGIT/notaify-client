da4c39e5479e75c05ac81ed9c70827cc0de872b5
rohit ranjan <rrohit2005@gmail.com>
2026-02-02 20:44:57 +0530

    refactor: update Project and ProjectConfig interfaces for consistency; add jsonwebtoken dependency

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,22 +1,24 @@
 export interface Project {
-    id: string;
-    name: string;
-    description: string;
-    createdAt: Date;
-    updatedAt: Date;
+  project_id: string;
+  user_id: string;
+  name: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface ProjectConfig {
-    id: string;
-    projectName: string;
-    llmType: "openai" | "claude" | "google" | "";
-    llmApiKey: string;
-    llmApiModel: string;
-    smtpUser: string;
-    smtpPass: string;
-    emailFrom: string;
-    emailTo: string;
-    notaifyApiKey?: string;
-    createdAt?: string;
-    updatedAt?: string;
+  project_id: string;
+  projectName: string;
+
+  llmType?: "openai" | "claude" | "google" | "";
+  llmApiKey?: string;
+  llmApiModel?: string;
+
+  smtpUser?: string;
+  smtpPass?: string;
+  emailFrom?: string;
+  emailTo?: string;
+
+  notaifyApiKey?: string;
 }
